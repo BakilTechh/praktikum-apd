25b6409cb8424bc255c3f6b5b11b7e18edc5c53c
BakilTechh <mhmmdbakilamru31@gmail.com>
2025-11-06 11:10:13 +0800

    Pertemuan 9

--- kelas/pertemuan-9/main.py
+++ kelas/pertemuan-9/main.py
@@ -47,19 +47,43 @@ import numpy as np
 # plt.show()
 
 # Data dua kelompok 
-fisika_kelompok1 = [85, 88, 90, 92, 87] 
-matematika_kelompok1 = [60, 58, 55, 62, 59] 
+# fisika_kelompok1 = [85, 88, 90, 92, 87] 
+# matematika_kelompok1 = [60, 58, 55, 62, 59] 
  
-fisika_kelompok2 = [65, 68, 70, 72, 69] 
-matematika_kelompok2 = [80, 85, 82, 88, 84] 
+# fisika_kelompok2 = [65, 68, 70, 72, 69] 
+# matematika_kelompok2 = [80, 85, 82, 88, 84] 
  
-plt.figure(figsize=(8, 5)) 
-plt.scatter(fisika_kelompok1, matematika_kelompok1, marker="o", label="Kelompok 1") 
-plt.scatter(fisika_kelompok2, matematika_kelompok2, marker="*", label="Kelompok 2") 
+# plt.figure(figsize=(8, 5)) 
+# plt.scatter(fisika_kelompok1, matematika_kelompok1, marker="o", label="Kelompok 1") 
+# plt.scatter(fisika_kelompok2, matematika_kelompok2, marker="*", label="Kelompok 2") 
  
-plt.title("Perbandingan Nilai Fisika dan Matematika") 
-plt.xlabel("Nilai Fisika") 
-plt.ylabel("Nilai Matematika") 
-plt.legend(loc="lower left") 
-plt.grid(True, alpha=0.5) 
-plt.show() 
+# plt.title("Perbandingan Nilai Fisika dan Matematika") 
+# plt.xlabel("Nilai Fisika") 
+# plt.ylabel("Nilai Matematika") 
+# plt.legend(loc="lower left") 
+# plt.grid(True, alpha=0.5) 
+# plt.show() 
+
+# import seaborn as sns 
+# # Mengambil dataset iris dari seaborn 
+# iris = sns.load_dataset("iris")
+
+# plt.figure(figsize=(8, 5)) 
+# # Membuat histogram untuk kolom sepal_length 
+# plt.hist(iris["sepal_length"], bins=15, color="skyblue", edgecolor="black", 
+# alpha=0.7) 
+# plt.title("Distribusi Panjang Sepal (sepal_length)") 
+# plt.xlabel("Sepal Length") 
+# plt.ylabel("Frekuensi") 
+# plt.grid(axis='y', alpha=0.5) 
+# plt.show() 
+
+views = [320, 450, 400, 390, 410, 470, 500, 490, 480, 510, 
+530, 600, 620, 590, 550, 570, 610, 640, 700, 5000, 1000, 2000, 800, 
+700, 750] 
+plt.figure(figsize=(7, 4)) 
+plt.boxplot(views, vert=False) 
+plt.xlabel('Number of Views') 
+plt.ylabel('Instagram') 
+plt.ylabel('Number of Costumers') 
+plt.grid(axis='y', alpha=0.5)
